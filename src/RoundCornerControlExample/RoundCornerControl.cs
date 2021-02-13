0492fd7ac9af6bcc614422210a52667612615c3a
Reza Aghaei <r.aghaei@outlook.com>
2021-02-14 00:15:21 +0800

    Update RoundCornerControl.cs

--- src/RoundCornerControlExample/RoundCornerControl.cs
+++ src/RoundCornerControlExample/RoundCornerControl.cs
@@ -23,23 +23,32 @@ namespace RoundCornerControlExample
                 this.RecreateRegion();
             }
         }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect,
+            int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
         private GraphicsPath GetRoundRectagle(Rectangle bounds, int radius)
         {
+            float r = radius;
             GraphicsPath path = new GraphicsPath();
-            path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
-            path.AddArc(bounds.X + bounds.Width - radius, bounds.Y, radius, radius, 270, 90);
-            path.AddArc(bounds.X + bounds.Width - radius, bounds.Y + bounds.Height - radius,
-                        radius, radius, 0, 90);
-            path.AddArc(bounds.X, bounds.Y + bounds.Height - radius, radius, radius, 90, 90);
-            path.CloseAllFigures();
+            path.StartFigure();
+            path.AddArc(bounds.Left, bounds.Top, r, r, 180, 90);
+            path.AddArc(bounds.Right - r, bounds.Top, r, r, 270, 90);
+            path.AddArc(bounds.Right - r, bounds.Bottom - r, r, r, 0, 90);
+            path.AddArc(bounds.Left, bounds.Bottom - r, r, r, 90, 90);
+            path.CloseFigure();
             return path;
         }
         private void RecreateRegion()
         {
-            var bounds = new Rectangle(this.ClientRectangle.Location, this.ClientRectangle.Size);
-            bounds.Inflate(-1, -1);
-            using (var path = GetRoundRectagle(bounds, this.Radius))
-                this.Region = new Region(path);
+            var bounds = ClientRectangle;
+            //using (var path = GetRoundRectagle(bounds, this.Radius))
+            //    this.Region = new Region(path);
+            
+            //Better round rectangle
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(bounds.Left, bounds.Top,
+                bounds.Right, bounds.Bottom, Radius, radius));
             this.Invalidate();
         }
         protected override void OnSizeChanged(EventArgs e)
